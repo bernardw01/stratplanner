908e5a1b2088aa640817537004ee0162c7603f77
bernardw01 <bernard.l.williams@live.com>
2017-09-02 18:46:59 -0400

    Initial Commit

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,6 +1,7 @@
 var mongojs = require("mongojs");
 var env = process.env.NODE_ENV || "dev";
-
+var MONGODB_URI = process.env.MONGODB_URI || null;
+mongodb://heroku_2s9g5g4x:hv9va9r4onbd5r39d79sb5bade@ds043082.mlab.com:43082/heroku_2s9g5g4x
 // Database configuration
 // Save the URL of our database as well as the name of our collection
 
@@ -14,10 +15,6 @@ var connection = {
     },
     prod: {
         databaseName: "stratplanner",
-        databaseUrl: "ds043082.mlab.com:43082",
-        collections: ["teams"],
-        user: "heroku_2s9g5g4x",
-        password: "Lexicon1"
     }
 };
 
@@ -28,7 +25,7 @@ var db = {};
 
 if (env == 'prod') {
     console.log("Attempting to connect to production Mongo Instance");
-    db = mongojs(prod.user + ':' + prod.password + "@" + prod.databaseUrl + "/" + prod.databaseName, prod.collections);
+    db = mongojs(MONGODB_URI);
 } else {
     console.log("Attempting to connect to development Mongo Instance");
     db = mongojs(dev.databaseName, dev.collections);
